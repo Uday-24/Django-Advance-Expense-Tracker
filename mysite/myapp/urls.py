4e6e8848f5c8401e4ea4e6245d3fe38ec4a12e69
Uday-24 <chotaliyauday24@gmail.com>
2024-11-08 18:23:40 +0530

    Edit and delete functionality added

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('fetch_expense/', views.fetch_expense, name="fetch_expense"),
+    path('update_expense/', views.update_expense, name="update_expense"),
+    path('delete_expense/', views.delete_expense, name="delete_expense"),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,11 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.db.models import Sum
 
 from .forms import ExpenseForm
+from .models import Expense
 # Create your views here.
 def index(request):
-    form = ExpenseForm()
 
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+            return redirect('index')
+
+
+    expenses = Expense.objects.all()
+    total_expense = expenses.aggregate(Sum('amount'))
+    form = ExpenseForm()
     context = {
-        'form':form
+        'form':form,
+        'expenses':expenses,
+        'total_expense':total_expense,
     }
     return render(request, 'myapp/index.html', context)
+
+def fetch_expense(request):
+    expense_id = request.GET.get('id')
+
+    expense = get_object_or_404(Expense, id = expense_id)
+
+    data = {
+        'name' : expense.name,
+        'amount' : expense.amount,
+        'category' : expense.category,
+    }
+
+    return JsonResponse(data)
+
+def update_expense(request):
+    if request.method == "POST":
+        id = request.POST.get('id')
+
+        expense = get_object_or_404(Expense, id = id)
+        
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({
+                'status': 'success',
+                'message' : 'Expense updated successfully'
+            })
+        else:
+            return JsonResponse({
+                'status': 'failed',
+                'message' : 'Expense is not updated'
+            })
+        
+    else:
+        return redirect('index')
+
+
+def delete_expense(request):
+    if request.method == "POST":
+        id = request.POST.get('id')
+        expense = get_object_or_404(Expense, id=id)
+        expense.delete()
+        data = {
+            'status': 'success',
+            'message': 'Expense deleted successfully'
+        }
+        return JsonResponse(data)
+    else:
+        return redirect('index')
